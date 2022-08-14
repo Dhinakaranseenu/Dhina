4cdc25875f8b2ae79e0828949c94fc8768505420
Dhinakaranseenu <109011799+Dhinakaranseenu@users.noreply.github.com>
2022-08-14 09:46:02 +0530

    Add files via upload

---
+++ car.java
@@ -0,0 +1,46 @@
+
+       class car{
+    String cars,color;
+     int price;
+    
+    public car() {
+     cars = "Kia";
+     price = 500000;
+     color = " blue";
+    }
+  public car(String a, int b,String c){
+       
+        this.cars=a;
+        this.price=b;
+        this.color=c;
+    }
+    
+    void write(){
+    
+        System.out.println("this is my new " +this.color+" "+this.cars+  " car which rupees. "+this.price+"\n");
+        
+    }
+    
+    public static void main(String[] args) {
+        
+       
+
+        car c = new car();
+       System.out.println(c.cars);
+       System.out.println(c.price);
+       System.out.println(c.color);
+        c.write();
+
+        car c1 = new car("honda",100000,"red");
+        System.out.println(c1.cars);
+        System.out.println(c1.price);
+        System.out.println(c1.color);
+         c1.write();
+
+         car c2 = new car("ford",120000,"green");
+        System.out.println(c2.cars);
+        System.out.println(c2.price);
+        System.out.println(c2.color);
+         c2.write();
+    }
+}
