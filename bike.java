c1103e16caff30bdf63cb959df7db0be72a4f113
Dhinakaranseenu <109011799+Dhinakaranseenu@users.noreply.github.com>
2022-08-14 11:50:00 +0530

    Add files via upload

---
+++ bike.java
@@ -0,0 +1,22 @@
+package A;
+import B.car;
+public class bike{
+
+    String type;
+    int price,num;
+
+    public bike(){
+        type ="honda";
+        price =520000;
+        num = 4549;
+        System.out.println(type + " " + price);
+
+
+
+
+    }
+    public static void main(String[] args){
+         car c5 =new car();
+    }
+}
+
--- car.java
+++ car.java
@@ -1,46 +1,54 @@
+package B;
 
-       class car{
+import A.bike;
+
+public class car{
     String cars,color;
-     int price;
-    
+    int price;
+
     public car() {
-     cars = "Kia";
-     price = 500000;
-     color = " blue";
+        cars = "Kia";
+        price = 500000;
+        color = " blue";
+        System.out.println("hi from B.car class");
     }
-  public car(String a, int b,String c){
-       
+    public car(String a, int b,String c){
+
         this.cars=a;
         this.price=b;
         this.color=c;
     }
-    
+
     void write(){
-    
-        System.out.println("this is my new " +this.color+" "+this.cars+  " car which rupees. "+this.price+"\n");
-        
+
+        System.out.println("this is my new " +this.color+" "+this.cars+  " B.car which rupees. "+this.price+"\n");
+
     }
-    
+
     public static void main(String[] args) {
-        
-       
+
+
 
         car c = new car();
-       System.out.println(c.cars);
-       System.out.println(c.price);
-       System.out.println(c.color);
+        System.out.println(c.cars);
+        System.out.println(c.price);
+        System.out.println(c.color);
         c.write();
 
         car c1 = new car("honda",100000,"red");
         System.out.println(c1.cars);
         System.out.println(c1.price);
         System.out.println(c1.color);
-         c1.write();
+        c1.write();
 
-         car c2 = new car("ford",120000,"green");
+        car c2 = new car("ford",120000,"green");
         System.out.println(c2.cars);
         System.out.println(c2.price);
         System.out.println(c2.color);
-         c2.write();
+        c2.write();
+
+        bike b = new bike();
+        System.out.println(b);
     }
 }
+
